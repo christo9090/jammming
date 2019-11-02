ffc98f31f8eb12a303fdbce77f8c7ad2e5e26961
Chris Scott <christo9090@gmail.com>
2019-11-02 11:23:33 -0600

    Updated file name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults'
 
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        <!-- Add a SearchBar component -->
+        <SearchBar />
         <div className="App-playlist">
           <!-- Add a SearchResults component -->
           <!-- Add a Playlist component -->
