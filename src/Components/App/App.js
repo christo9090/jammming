882c5d5e2a0d57e4af51dff365ca42bc8af7c11a
Chris Scott <christo9090@gmail.com>
2019-11-02 12:42:05 -0600

    Passed state of search results down to the track list

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,21 +2,48 @@ import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults'
+import Playlist from '../Playlist/Playlist'
 
-
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: "Name1",
+          artist: "artist1",
+          album: "album1",
+          id: "id1"
+        },
+        {
+          name: "Name2",
+          artist: "artist2",
+          album: "album2",
+          id: "id2"
+        },
+        {
+          name: "Name3",
+          artist: "artist3",
+          album: "album3",
+          id: "id3"
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
