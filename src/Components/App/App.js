306cfb082ce9aa039451688b0a590832055c3482
Chris Scott <christo9090@gmail.com>
2019-11-03 15:53:36 -0700

    Added same button basic functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -47,6 +47,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this)
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
+    this.savePlaylist = this.savePlaylist.bind(this)
   }
 
   addTrack(track) {
@@ -81,6 +82,14 @@ class App extends React.Component {
     })
   }
 
+  savePlaylist() {
+    let trackURIs = []
+    this.state.playlistTracks.map(track => {
+      trackURIs.push(track.id)
+    })
+    console.log(trackURIs)
+  }
+
   render() {
     return (
       <div>
@@ -89,7 +98,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
--- src/Components/PlayList/Playlist.js
+++ src/Components/PlayList/Playlist.js
@@ -12,12 +12,14 @@ class Playlist extends React.Component {
     this.props.onNameChange(event.target.value)
   }
 
+
+
   render() {
     return (
       <div className="Playlist">
         <input defaultValue={this.props.playlistName}  onChange={this.handleNameChange}/>
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval="true"/>
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
       </div>
     );
   }
